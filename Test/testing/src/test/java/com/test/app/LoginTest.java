f669fd4ce8b3d17adc33e003476cb520a392fd01
Nikita Bansode <bansodenikita66@gmail.com>
2025-09-17 18:56:15 +0530

    testing

---
+++ Test/testing/src/test/java/com/test/app/LoginTest.java
@@ -0,0 +1,60 @@
+package com.test.app;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginTest {
+    
+WebDriver driver;
+
+@BeforeClass
+public void beforeMethod(){
+   WebDriverManager.chromedriver().setup();
+   driver=new ChromeDriver();
+   driver.manage().window().maximize();
+}
+
+@Test
+public void testTitle(){
+    driver.get("http://127.0.0.1:5500/src/main/webpages/login.html");
+    String title=driver.getTitle();
+    Assert.assertEquals(title,"login page");
+}
+
+@Test
+public void testName(){
+    driver.get("http://127.0.0.1:5500/src/main/webpages/login.html");
+    WebElement inputName=driver.findElement(By.name("username"));
+    Assert.assertTrue(inputName.isDisplayed());
+}
+
+@Test
+public void testPassword(){
+    driver.get("http://127.0.0.1:5500/src/main/webpages/login.html");
+    WebElement pass=driver.findElement(By.name("password"));
+    Assert.assertTrue(pass.isDisplayed());
+}
+
+@Test
+public void loginSuccess(){
+    driver.get("http://127.0.0.1:5500/src/main/webpages/login.html");
+    driver.findElement(By.name("password")).sendKeys("Nikita@123");
+    driver.findElement(By.name("username")).sendKeys("Nikita");
+    driver.findElement(By.name("submit")).click();
+
+    WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(3));
+    wait.until(ExpectedConditions.urlContains(null))
+}
+}
