3e0a72fac400a35aaef006ba0c5f2bc7f130dbbc
Nikita Bansode <bansodenikita66@gmail.com>
2025-10-03 15:08:29 +0530

    End to end testing for the product

---
+++ Test/EndToEndTesting/src/main/java/com/tap/EndToEndTesting/EndToEndTestingApplication.java
@@ -0,0 +1,13 @@
+package com.tap.EndToEndTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndToEndTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndToEndTestingApplication.class, args);
+	}
+
+}
---
+++ Test/EndToEndTesting/src/test/java/com/tap/EndToEndTesting/EndToEndTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap.EndToEndTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndToEndTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
