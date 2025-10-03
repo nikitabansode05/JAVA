661ecd427ac155e1305bb00b1a66f2df47fc3e98
Nikita Bansode <bansodenikita66@gmail.com>
2025-10-03 15:21:43 +0530

    Product Controller

---
+++ Test/EndToEndTesting/src/main/java/com/tap/EndToEndTesting/Controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.tap.EndToEndTesting.Controller;
+
+import com.tap.EndToEndTesting.Entity.Product;
+import com.tap.EndToEndTesting.Service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    // private final List<Product> products = new ArrayList<>(Arrays.asList(
+    // new Product(1, "Laptop", 75000),
+    // new Product(2, "Phone", 35000),
+    // new Product(3, "Headphones", 2000)
+    // ));
+
+    private ProductService _service;
+
+    @Autowired
+    public ProductController(ProductService service) {
+        _service = service;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return _service.getAllList();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return _service.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return _service.createProduct(product);
+        // Product createdProduct = _service.createProduct(product);
+        // return ResponseEntity
+        //         .status(201) // HTTP 201 Created
+        //         .body(createdProduct);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
+        return _service.updateProduct(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable int id) {
+       return _service.deleteProduct(id);
+    }
+}
