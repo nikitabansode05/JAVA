88de3af2635b7844ada257d558d23f4891b68352
Nikita Bansode <bansodenikita66@gmail.com>
2025-10-07 16:20:25 +0530

    Assessment Test

--- Test/TFLAssessmentTesting/src/test/java/com/tap/testing/LoginTest.java
+++ Test/TFLAssessmentTesting/src/test/java/com/tap/testing/LoginTest.java
@@ -1,18 +1,17 @@
 package com.tap.testing;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -39,10 +38,24 @@ public class LoginTest{
         WebElement pass=driver.findElement(By.id("password"));
 
         mail.sendKeys("nikita.bansode@example.com");
-        Assert.assertEquals("nikita.bansode@example.com",mail.getAttribute("value"));
         pass.sendKeys("12345");
+        Assert.assertEquals("nikita.bansode@example.com",mail.getDomProperty("value"));
+        Assert.assertEquals("12345", pass.getDomProperty("value"));
         driver.findElement(By.cssSelector("button[type='submit']")).click();
+        System.out.println("Student logged in");
 
         wait.until(ExpectedConditions.urlContains("/Student/student.html"));
+        driver.findElement(By.linkText("Get All Test")).click();
+
+        //driver.findElement(By.xpath("//button[@data-id='3']")).click();
+       //driver.findElement(By.cssSelector("button[data-id='3']").click();
+
+        // WebElement btn = driver.findElement(By.cssSelector("button[data-id='3']"));
+        // btn.click();
+
+        // WebElement detailsBtn = driver.findElement(By.xpath("//button[contains(@data-test, 'Java')]"));
+        // detailsBtn.click();
+
+        driver.findElement(By.xpath("//button[@class='detailsBtn' and contains(@data-test, 'Java')]")).click();
     }
 }
