0c9f4f0c46ff1885d031e7592f758da8515e554a
Nikita Bansode <bansodenikita66@gmail.com>
2025-11-24 17:24:25 +0530

    Login test

---
+++ Test/AssessmentAPITesting/src/main/java/com/tap/testing/AssessmentAPITesting/User.java
@@ -0,0 +1,24 @@
+package com.tap.testing.AssessmentAPITesting;
+
+public class User {
+    private String email;
+    private String password;
+
+    public User(){
+        email=null;
+        password=null;
+    }
+
+    public User(String email,String password){
+        this.email=email;
+        this.password=password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
+
--- Test/AssessmentAPITesting/src/test/java/com/tap/testing/AssessmentAPITesting/AuthControllerTest.java
+++ Test/AssessmentAPITesting/src/test/java/com/tap/testing/AssessmentAPITesting/AuthControllerTest.java
@@ -7,32 +7,74 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 import io.restassured.http.ContentType;
+// import tools.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
 
 public class AuthControllerTest {
     
     @Test
     public void loginTest(){
-
         RestAssured.baseURI="http://localhost:5238";
+        ObjectMapper mapper=new ObjectMapper();
+        List<User> userList=new ArrayList<User>();
+       
+        System.out.println("***************************************************************************************");
+        try {
+                    userList = mapper.readValue(
+                    new File("src/main/resources/loginSample.json"),
+                    new TypeReference<List<User>>() {}
+                );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        
+            
+            for(User u : userList) {
+                System.out.println(u.getEmail()+ " " + u.getPassword());
+                System.out.println("***************************************************************************************");
+            
+            // String loginCredentials= """
+            //     {
+            //         "email":userList.get(i.getEmail()),
+            //         "password":userList.get(i.getPassword())
+            //     }
+            //     """;
 
-        String loginCredentials= """
+            // String loginCredentials= """
+            //     {
+            //         "email":"nikita.bansode@example.com",
+            //         "password":"12345"
+            //     }
+            //     """;
+            String loginCredentials = """
                 {
-                    "email":"nikita.bansode@example.com",
-                    "password":"12345"
+                    "email":"%s",
+                    "password":"%s"
                 }
-                """;
-        given()
-            .contentType(ContentType.JSON)
-            .body(loginCredentials)
-        .when()
-            .post("api/auth/login")
-        .then()
-         .statusCode(200)
-        .body("user.email",equalTo("nikita.bansode@example.com"))
-        .body("user.firstname",equalTo("nikita"))
-        .body("user.lastname",equalTo("bansode"))
-        .body("user.id",equalTo(7))
-        .body("user.userRoles[0].role.name",equalTo("student"));
+                """.formatted(u.getEmail(), u.getPassword());
+
+            System.out.println(loginCredentials);
+
+            given()
+                .contentType(ContentType.JSON)
+                .body(loginCredentials)
+            .when()
+                .post("api/auth/login")
+            .then()
+            .statusCode(200);
+            // .body("user.email",equalTo(sample.email));
+            // .body("user.firstname",equalTo("nikita"))
+            // .body("user.lastname",equalTo("bansode"))
+            // .body("user.id",equalTo(7))
+            // .body("user.userRoles[0].role.name",equalTo("student"));
+        }
+    
     }
 
     @Test
@@ -46,7 +88,7 @@ public class AuthControllerTest {
                 "email":"ui.ux@gmail.com",
                 "contactNumber":"8907654321",
                 "password":"12345"
-}
+            }
                 """;
         given()
             .contentType(ContentType.JSON)
