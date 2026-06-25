08260bf196d8a6aa812cb4a7acb10120f6f49d8c
Nikita Bansode <bansodenikita66@gmail.com>
2026-06-25 12:35:47 +0530

    Interface Saggrigation Applied

--- Simple/Polymorphism/Overriding/TransactionManagement/src/main/java/com/banking/transaction/accountManagers/ITransactionService.java
+++ Simple/Polymorphism/Overriding/TransactionManagement/src/main/java/com/banking/transaction/accountManagers/ITransactionService.java
@@ -6,12 +6,6 @@ import java.util.List;
 import com.banking.transaction.entities.Operation;
 
 public interface ITransactionService {
-    void debit(double amount,int accountNo);
-    void credit(double amount,int accountNo);
-    void transation(int accountNo1,int accountNo2,double amount);
-    void getAccountDetails(int accountNo);
-    void createAccount();
-    List<Operation> getStatement(int accountNo);
     double calculateInterest(int accountNo,double interest);
     double applyInterest(int accountNo,double interest);
     void applyInteresttoAll(double interest);
